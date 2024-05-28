3f75c0c1fe4116570c69b835cd1053f873dad5a6
QuatschVirus <quatschvirus@gmail.com>
2024-05-28 23:16:21 +0200

    Device code is maybe finished, began work on the driver

--- Device/Device.ino
+++ Device/Device.ino
@@ -1,57 +1,100 @@
+#include <Firmata.h>
+
 #include <RotaryEncoder.h>
 #include <LiquidCrystal_I2C.h>
 #include <Adafruit_MCP23X17.h>
 
+#define EncoderMode RotaryEncoder::LatchMode::FOUR0
+
 LiquidCrystal_I2C display(0x27, 20, 4);
-Adafruit_MCP23X17 knobs;
+Adafruit_MCP23X17 leds;
 Adafruit_MCP23X17 buttons;
 
-long command;
+RotaryEncoder encoder1(2, 3, EncoderMode);
+RotaryEncoder encoder2(4, 5, EncoderMode);
+RotaryEncoder encoder3(6, 7, EncoderMode);
+RotaryEncoder encoder4(8, 9, EncoderMode);
+
+const uint8_t PACKET_COMMAND = 0xAA;
+
+void firmata_callback(char* input) {
+  // Recieving
+  int cmd = 0;
+
+  char* ptr = strtok(input, "\n");
+  byte i = 0;
+  while (ptr) {
+    if (i < 1) {
+      cmd = atol(ptr);
+    } else {
+      display.setCursor(0, i - 1);
+      display.print(ptr);
+    }
+    ptr = strtok(NULL, "\n");
+    i++;
+  }
+
+  // for (int i = 0; i < 14; i++) {
+  //   if (((1 << i) & cmd) != 0) {
+  //     leds.digitalWrite(i, HIGH);
+  //   } else {
+  //     leds.digitalWrite(i, LOW);
+  //   }
+  // }
+
+  // Sending
+  int state = 0;
+  // for (int i = 0; i < 14; i++) {
+  //   state |= ((~buttons.digitalRead(i)) << i);
+  // }
+
+  char enc1 = min(-128, max(encoder1.getPosition(), 127)) & 0xFF;
+  char enc2 = min(-128, max(encoder2.getPosition(), 127)) & 0xFF;
+  char enc3 = min(-128, max(encoder3.getPosition(), 127)) & 0xFF;
+  char enc4 = min(-128, max(encoder4.getPosition(), 127)) & 0xFF;
+
+  encoder1.setPosition(0);
+  encoder2.setPosition(0);
+  encoder3.setPosition(0);
+  encoder4.setPosition(0);
+
+  char stateBig = (state >> 8) & 0xFF;
+  char stateSmall = state & 0xFF;
+  char response[7] = {stateBig, stateSmall, enc1, enc2, enc3, enc4, '\0'};
+  Firmata.sendString(response);
+}
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
-  Serial.write("AeroFace_Autopilot");
 
-  buttons.begin_I2C();
+  buttons.begin_I2C(0x20);
+  leds.begin_I2C(0x22);
 
   display.init();
+  display.backlight();
 
   display.setCursor(0, 0);
-  display.println("Welcome to AeroFace!");
-  display.println("  Autopilot Module  ");
-  display.println("  Awaiting input... ");
-  display.println("                    ");
+  display.print("Welcome to AeroFace!");
+  display.setCursor(0, 1);
+  display.print("  Autopilot Module  ");
+  display.setCursor(0, 2);
+  display.print("  Awaiting input... ");
+  display.setCursor(0, 3);
+  display.print("                    ");
 
-  // TODO: Pin setup
+  Firmata.setFirmwareVersion(FIRMATA_FIRMWARE_MAJOR_VERSION, FIRMATA_FIRMWARE_MINOR_VERSION);
+  Firmata.attach(STRING_DATA, firmata_callback);
+  Firmata.begin(57600);
+
+  // for (int i = 0; i < 13; i++) {
+  //   leds.pinMode(i, OUTPUT);
+  //   buttons.pinMode(i, INPUT_PULLUP);
+  // }
+
+  Serial.println("AeroFace_Autopilot");
 }
 
 void loop() {
-  if (Serial.available()) {
-    int leds = Serial.read();
-    display.println(Serial.readStringUntil('\n'));
-    display.println(Serial.readStringUntil('\n'));
-    display.println(Serial.readStringUntil('\n'));
-    display.println(Serial.readStringUntil('\n'));
-
-    for (int i = 0; i < 9; i++) {
-      if ((leds & (1 << i)) != 0) {
-        digitalWrite(i + 2, HIGH);
-      } else {
-        digitalWrite(i + 2, LOW);
-      }
-    }
-
-    for (int i = 0; i < 4; i++) {
-      if ((leds & (1 << (i + 9))) != 0) {
-        knobs.digitalWrite((i * 4) + 3, HIGH);
-      } else {
-        knobs.digitalWrite((i * 4) + 3, LOW);
-      }
-    }
-  }
-
-  for (int i = 0; i < 9; i++) {
-    command |= buttons.digitalRead(i) << i;
-  }
+  
 }
--- Driver/Program.cs
+++ Driver/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Driver
+﻿using Solid.Arduino;
+
+namespace Driver
 {
-    internal class Program
+    public class Program
     {
+        static ArduinoSession s;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (s = new(new SerialConnection("COM6", SerialBaudRate.Bps_115200)))
+            {
+                Console.WriteLine(s.ReadLine());
+                s.StringReceived += Process;
+                s.SendStringData("0\n" + FormatForDisplay("Welcome to AeroFace!", "Autopilot Module", "Connected!"));
+                Console.ReadKey(true);
+            }
+        }
+
+        private static void Process(object sender, StringEventArgs eventArgs)
+        {
+            Console.WriteLine(eventArgs.Text.Length);
+        }
+
+        public static string FormatForDisplay(params string[] lines)
+        {
+            string output = "";
+            foreach (string line in lines) {
+                if (line.Length > 20) continue;
+                string padding = string.Concat(Enumerable.Repeat(' ', (20 - line.Length) / 2));
+                output += padding + line + padding + "\n";
+            }
+            return output.TrimEnd('\n');
         }
     }
 }
